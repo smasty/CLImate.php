0cb68407fb10c46bb25468e85205cc3bd81fd94a
Smasty <martin@smasty.net>
2012-04-06 17:18:22 +0200

    Add Dots notificator (Loading...)

---
+++ CLImate/Notificators/Dots.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright 2012 Martin Srank (http://smasty.net)
+ */
+
+namespace CLImate\Notificators;
+
+use CLImate;
+
+
+/**
+ * Notificator showing string of dots.
+ * @todo printf named args
+ */
+class Dots extends CLImate\Notificator {
+
+
+	/** @var string */
+	protected $format = "%s %s  (%s,  %d/s)"; // {message} {dots}  ({elapsed}, {speed}/s)
+
+	/** @var int */
+	protected $dots;
+
+	private $i = 0;
+
+
+	public function __construct($message, $dots = 3, $interval = 300){
+		$this->dots = (int) $dots + 1;
+		parent::__construct($message, $interval);
+		if($dots <= 0)
+			throw new \InvalidArgumentException('Number of dots must be positive.');
+	}
+
+
+	public function display(){
+		$i = $this->last ? $this->dots-1 : $this->i++ % $this->dots;
+		$dots = str_pad(str_repeat('.', $i), $this->dots);
+		printf($this->format, $this->message, $dots, $this->formatTime($this->elapsed()), round($this->speed()));
+	}
+
+
+}
