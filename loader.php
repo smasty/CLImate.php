6bf85959210d3fc126daeac3781064bd465a0c4a
Smasty <martin@smasty.net>
2012-04-14 13:23:19 +0200

    Autoloader: use anonymous function

--- loader.php
+++ loader.php
@@ -4,13 +4,10 @@
  */
 
 
-function CLImateAutoload($class){
+spl_autoload_register(function($class){
 	$class = trim($class, '\\');
 	$file = __DIR__ . '/' . strtr($class, '\\', '/') . '.php';
 	if(substr($class, 0, 8) === 'CLImate\\' && is_file($file)){
 		include_once $file;
 	}
-}
-
-
-spl_autoload_register('CLImateAutoload');
+});
