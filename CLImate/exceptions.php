7f68542a2035fcc874ced11eff2158d79656b10b
Smasty <martin@smasty.net>
2012-04-14 13:36:21 +0200

    Custom exceptions

---
+++ CLImate/exceptions.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright 2012 Martin Srank (http://smasty.net)
+ */
+
+namespace CLImate;
+
+
+/**
+ * Exception thrown when an error occures on input.
+ */
+class InputException extends \Exception {}
