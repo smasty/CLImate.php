3ed8cee3f577818dda39b25da3a17167f6ae6fb0
Smasty <martin@smasty.net>
2012-05-01 13:56:04 +0200

    Exceptions: Ultimate ancestor: CLImate\CLImateException

--- src/CLImate/exceptions.php
+++ src/CLImate/exceptions.php
@@ -7,6 +7,18 @@ namespace CLImate;
 
 
 /**
+ * General CLImate exception.
+ */
+class CLImateException extends \Exception {}
+
+
+/**
  * Exception thrown when an error occures on input.
  */
-class InputException extends \Exception {}
+class InputException extends CLImateException {}
+
+
+/**
+ * CLImate\Application-specific exception.
+ */
+class ApplicationException extends CLImateException {}
